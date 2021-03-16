3d394ce09efa6d8a29722ebb77902ccc8299da87
Azim Khakulov <azim@khakulov.com>
2021-03-16 18:51:03 +0500

    fix

--- auth/serializers.py
+++ auth/serializers.py
@@ -1,11 +1,8 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
-    token = serializers.SerializerMethodField()
-
     class Meta:
         model = get_user_model()
         fields = [
@@ -13,7 +10,3 @@ class UserSerializer(serializers.ModelSerializer):
             'education_year',
         ]
         read_only_fields= [ 'id' ]
-
-    def get_token(self, obj):
-        token, created = Token.objects.get_or_create(user=obj)
-        return token.key
