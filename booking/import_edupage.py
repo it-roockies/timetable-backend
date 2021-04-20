98c59c22ca96285548e5a0de5c95507fdaac6c53
Azim Khakulov <azim@khakulov.com>
2021-04-20 21:15:51 +0500

    filter for week

---
+++ booking/import_edupage.py
@@ -0,0 +1,129 @@
+import logging
+import requests
+
+from datetime import date, timedelta, datetime
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from . import models
+
+
+logger = logging.getLogger(__name__)
+
+
+def request(*, method: str, url: str, data=None):
+    try:
+        response = requests.request(method=method, url=url, json=data)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        logger.exception(str(e))
+        if hasattr(e, "response"):
+            logger.debug(e.response.text)
+        return None
+
+
+def get_timetables():
+    data = request(
+        method="POST",
+        url="https://ttpu.edupage.org/timetable/server/ttviewer.js?__func=getTTViewerData",
+        data={"__args": [None, 2020], "__gsh": "00000000"},
+    )
+    return data["r"]["regular"]["timetables"]
+
+
+def get_timetable_data(tt_num):
+    data = request(
+        method="POST",
+        url="https://ttpu.edupage.org/timetable/server/regulartt.js?__func=regularttGetData",
+        data={"__args": [None, tt_num], "__gsh": "00000000"},
+    )
+    return data["r"]["dbiAccessorRes"]["tables"]
+
+
+DAYS_MAP = {
+    "100000": 0,
+    "010000": 1,
+    "001000": 2,
+    "000100": 3,
+    "000010": 4,
+    "000001": 5,
+}
+
+
+"""returns date according to week number. 0 is monday (starts from current week's day)"""
+
+
+def get_date_for_day(day, week: str):
+    start = datetime.strptime(f"{week}-1", "%Y-W%W-%w").date()  # starting of week's date
+    result = start + timedelta(days=DAYS_MAP[day])
+    return result
+
+
+def delete_week_cards(week: str):
+    start = datetime.strptime(f"{week}-1", "%Y-W%W-%w").date()
+    end = start + timedelta(days=7)
+    bookings = models.Booking.objects.filter(date__range=[start, end])
+    bookings.delete()
+
+
+"""takes file and stores information into database"""
+
+
+def import_timetable():
+    timetables = get_timetables()
+    tt_num = timetables[0]["tt_num"]
+    data = get_timetable_data(tt_num)
+
+    # delete_week_cards(week)
+
+    # Import Subjects
+    _subjects = next(table["data_rows"] for table in data if table["id"] == "subjects")
+    subjects = {[subject["id"]]: models.Subject.objects.get(short=subject["short"]) for subject in _subjects}
+
+    # Import Teachers
+    _teachers = next(table["data_rows"] for table in data if table["id"] == "teachers")
+    teachers = {[teacher["id"]]: models.Teacher.objects.get(short=teacher["short"]) for teacher in _teachers}
+
+    # import Groups
+    _groups = next(table["data_rows"] for table in data if table["id"] == "classes")
+    groups = {[group["id"]]: models.Group.objects.get(short=group["short"]) for group in _groups}
+
+    # import Classrooms
+    _classrooms = next(table["data_rows"] for table in data if table["id"] == "classrooms")
+    classrooms = {
+        [classroom["id"]]: models.Classroom.objects.get(short=classroom["short"]) for classroom in _classrooms
+    }
+
+    # import Lessons
+    _lessons = next(table["data_rows"] for table in data if table["id"] == "lessons")
+    lessons = {}
+    for _lesson in _lessons:
+        lesson_subject = subjects[_lesson["subjectid"]]
+        lesson_teachers = [teachers[_id] for _id in _lesson["teacherids"]]
+        lesson_groups = [groups[_id] for _id in _lesson["classids"]]
+
+        lessons[_lesson["id"]] = (
+            models.Lesson.objects.filter(subject=lesson_subject)
+            .filter(teachers__in=lesson_teachers)
+            .annotate(num_teachers=Count("teachers"))
+            .filter(num_teachers=len(lesson_teachers))
+            .filter(groups__in=lesson_groups)
+            .annotate(num_groups=Count("groups"))
+            .filter(num_groups=len(lesson_groups))
+            .get()
+        )
+
+    # import Booking
+    for child in root.iter("card"):
+        booking_date = get_date_for_day(day=child.attrib["days"], week=week)
+        period = child.attrib["period"]
+        classroom_id = child.attrib["classroomids"]
+        lesson_id = child.attrib["lessonid"]
+        lesson_object = models.Lesson.objects.get(lesson_id=lesson_id)
+        if classroom_id:
+            classroom_object = models.Classroom.objects.get(classroom_id=classroom_id)
+            models.Booking.objects.get_or_create(
+                date=booking_date, period=period, lesson=lesson_object, classroom=classroom_object
+            )
+        else:
+            models.Booking.objects.get_or_create(date=booking_date, period=period, lesson=lesson_object, classroom=None)
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime, date, timedelta
 
 from django.core.cache import cache
@@ -24,50 +23,65 @@ from . import models
 from . import filters
 from django.db.models import Max
 
+
 def last_modified(request, *args, **kwargs):
-    return cache.get_or_set('last_modified', datetime.utcnow())
+    return cache.get_or_set("last_modified", datetime.utcnow())
+
 
 class CachedReadOnlyModelViewSet(ReadOnlyModelViewSet):
     @method_decorator(condition(last_modified_func=last_modified))
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
+
 class BookingViewSet(CachedReadOnlyModelViewSet):
     """Interacts with booking"""
-    permission_classes = (AllowAny, )
+
+    permission_classes = (AllowAny,)
     queryset = models.Booking.objects.all()
     serializer_class = serializers.BookingSerializer
-    filter_backends = (filters.BookingWeekFilter, )
+    filter_backends = (filters.BookingWeekFilter,)
+
 
 class TeacherViewSet(CachedReadOnlyModelViewSet):
     """Interacts with teachers"""
-    permission_classes = (AllowAny, )
+
+    permission_classes = (AllowAny,)
     queryset = models.Teacher.objects.all()
     serializer_class = serializers.TeacherSerializer
 
+
 class GroupViewSet(CachedReadOnlyModelViewSet):
     """Interacts with groups"""
-    permission_classes = (AllowAny, )
+
+    permission_classes = (AllowAny,)
     queryset = models.Group.objects.all()
     serializer_class = serializers.GroupSerializer
 
+
 class ClassroomViewSet(CachedReadOnlyModelViewSet):
     """Interacts with rooms"""
-    permission_classes = (AllowAny, )
+
+    permission_classes = (AllowAny,)
     queryset = models.Classroom.objects.all()
     serializer_class = serializers.ClassroomSerializer
 
+
 class SubjectViewSet(CachedReadOnlyModelViewSet):
     """Interacts with rooms"""
-    permission_classes = (AllowAny, )
+
+    permission_classes = (AllowAny,)
     queryset = models.Subject.objects.all()
     serializer_class = serializers.SubjectSerializer
 
+
 class MessageViewSet(ReadOnlyModelViewSet):
     """returns all available messages """
+
     authentication_classes = [TelegramBotAuthentication]
     queryset = models.Message.objects.all()
     serializer_class = serializers.MessageSerializer
+
 
 class TimeTablePermission(BasePermission):
     """
@@ -75,23 +89,34 @@ class TimeTablePermission(BasePermission):
     """
 
     def has_permission(self, request, view):
-        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+        if request.method in ("GET", "HEAD", "OPTIONS"):
             return True
         return bool(request.user and request.user.is_staff)
 
 
 period_in_minutes = {0: 0, 1: 600, 2: 680, 3: 780, 4: 860, 5: 940, 6: 1020, 7: 1100}
-period_in_time = {1: "09:00-10:00", 2: "10:20-11:20", 3: "12:00-13:00", 4: "13:20-14:20", 5: "14:40-15:40", 6: "16:00-17:00", 7: "17:20-18:20"}
+period_in_time = {
+    1: "09:00-10:00",
+    2: "10:20-11:20",
+    3: "12:00-13:00",
+    4: "13:20-14:20",
+    5: "14:40-15:40",
+    6: "16:00-17:00",
+    7: "17:20-18:20",
+}
+
+
 class GroupLessonViewSet(ViewSet):
     """returns lessons to the user for given date"""
+
     # authentication_classes = []
     # permission_classes = []
     def list(self, request):
-        date = request.data.get('date')  # date of the lesson for a particular group
-        group = request.data.get('group')  # group
-        minutes = request.data.get('minutes')
+        date = request.data.get("date")  # date of the lesson for a particular group
+        group = request.data.get("group")  # group
+        minutes = request.data.get("minutes")
         bookings = models.Booking.objects.filter(date=date)  # filtering bookings through data
-        now_lesson = {"message": 'You are not supposed to be in any lesson right now'}
+        now_lesson = {"message": "You are not supposed to be in any lesson right now"}
         cards = []
         for booking in bookings:  # given data's bookings
             try:
@@ -105,7 +130,7 @@ class GroupLessonViewSet(ViewSet):
                 period = booking.period
                 subject = booking.lesson.subject.short
                 teachers = booking.lesson.teachers.all()
-                teacher = ''
+                teacher = ""
                 if len(teachers) != 1:
                     for t in teachers:
                         teacher += t.short
@@ -115,144 +140,169 @@ class GroupLessonViewSet(ViewSet):
                     classroom = booking.classroom.name
                 except:
                     classroom = None
-                card = dict(period=period,
-                            date=date,
-                            classroom=classroom,
-                            group=group,
-                            teacher=teacher,
-                            subject=subject)
+                card = dict(
+                    period=period,
+                    date=date,
+                    classroom=classroom,
+                    group=group,
+                    teacher=teacher,
+                    subject=subject,
+                    time=period_in_time[int(card["period"])],  # get time for lesson
+                )
                 cards.append(card)
-                if period_in_minutes[int(card['period'])-1] <= int(minutes) <= period_in_minutes[int(card['period'])]:
+                if period_in_minutes[int(card["period"]) - 1] <= int(minutes) <= period_in_minutes[int(card["period"])]:
                     now_lesson = card
-                card['time'] = period_in_time[int(card['period'])]  # get time for lesson
-        if len(cards) == 0:
-            msg = {'message': "Today you have no classes"}
-            return Response(msg, status=status.HTTP_200_OK)
-        # sorting lessons by period
-        cards = sorted(cards, key=lambda card: card['period'])
 
-        return Response({
-            'today_lessons': cards,
-            'now_lesson': now_lesson
-        }, status=status.HTTP_200_OK)
+        if len(cards) == 0:
+            msg = {"message": "Today you have no classes"}
+            return Response(msg, status=status.HTTP_200_OK)
+
+        # sorting lessons by period
+        cards = sorted(cards, key=lambda card: card["period"])
+        return Response({"today_lessons": cards, "now_lesson": now_lesson}, status=status.HTTP_200_OK)
+
 
 class NotifyUserViewSet(ViewSet):
     """helps to notify student about lesson"""
+
     authentication_classes = [TelegramBotAuthentication]
+
     def list(self, request):
-        period = request.data.get('period')
-        today = request.data.get('date')
+        period = request.data.get("period")
+        today = request.data.get("date")
         bookings = models.Booking.objects.filter(date=today, period=period)
         data = []
         for booking in bookings:  # all lessons for a given period
             ready_dic = {}
-            ready_dic['teachers'] = [teacher.short for teacher in booking.lesson.teachers.all()]  # getting teacher
-            ready_dic['subject'] = booking.lesson.subject.short
-            ready_dic['telegram_ids'] = [user.telegram_id for user in models.User.objects.filter(group__in=booking.lesson.groups.all(), telegram_id__isnull=False)]
+            ready_dic["teachers"] = [teacher.short for teacher in booking.lesson.teachers.all()]  # getting teacher
+            ready_dic["subject"] = booking.lesson.subject.short
+            ready_dic["telegram_ids"] = [
+                user.telegram_id
+                for user in models.User.objects.filter(group__in=booking.lesson.groups.all(), telegram_id__isnull=False)
+            ]
             data.append(ready_dic)
         return Response(data, status=status.HTTP_200_OK)
 
+
 class TimeTableViewSet(ViewSet):
-    permission_classes = (TimeTablePermission, )
+    permission_classes = (TimeTablePermission,)
 
     """ Returns table date for current week """
+
     def list(self, request):
         filter_kwars = {}
-        if 'group' in request.query_params:
-            filter_kwars['lesson__groups__id__in'] = request.query_params.getlist('group')
+        if "group" in request.query_params:
+            filter_kwars["lesson__groups__id__in"] = request.query_params.getlist("group")
 
-        if 'teacher' in request.query_params:
-            filter_kwars['lesson__teachers__id__in'] = request.query_params.getlist('teacher')
+        if "teacher" in request.query_params:
+            filter_kwars["lesson__teachers__id__in"] = request.query_params.getlist("teacher")
 
-        if 'classroom' in request.query_params:
-            filter_kwars['classroom_id__in'] = request.query_params.getlist('classroom')
-            print(request.query_params.getlist)
-        if 'date' in request.query_params:
-            filter_kwars['date'] = request.query_params.get('date')
+        if "classroom" in request.query_params:
+            filter_kwars["classroom_id__in"] = request.query_params.getlist("classroom")
 
-        elif 'week' in request.query_params:
-            week = request.query_params.get('week')
-            if week == 'current':
+        if "date" in request.query_params:
+            filter_kwars["date"] = request.query_params.get("date")
+
+        elif "week" in request.query_params:
+            week = request.query_params.get("week")
+            if week == "current":
                 tomorrow = date.today() + timedelta(days=1)
                 start_date = tomorrow - timedelta(days=tomorrow.weekday())
-                end_date = start_date + timedelta(days=6)
-                filter_kwars['date__range'] = [start_date, end_date]
-
-        if 'date' not in filter_kwars and 'date__range' not in filter_kwars:
-            today = date.today() # get today's date
-            if today.weekday() == 6:
-                filter_kwars['date'] = today + timedelta(days=1)
             else:
-                filter_kwars['date'] = today
+                start_date = datetime.strptime(f"{week}-1", "%Y-W%W-%w").date()
+            end_date = start_date + timedelta(days=6)
+            filter_kwars["date__range"] = [start_date, end_date]
 
-        bookings = [{
-            "id": booking.id,
-            "lesson_id": booking.lesson.id,
-            "date": booking.date,
-            "period": booking.period,
-            "subject": {
-                "id": booking.lesson.subject.id,
-                "name": booking.lesson.subject.short,
-            },
-            "teachers": [{
-                "id": teacher.id,
-                "name": teacher.short,
-            } for teacher in booking.lesson.teachers.all()],
-            "groups": [{
-                "id": group.id,
-                "name": group.name,
-            } for group in booking.lesson.groups.all()],
-            "classroom": {
-                "id": booking.classroom.id,
-                "name": booking.classroom.name,
-            } if booking.classroom else None,
-        } for booking in models.Booking.objects.filter(**filter_kwars).order_by('date')]
+        if "date" not in filter_kwars and "date__range" not in filter_kwars:
+            today = date.today()  # get today's date
+            if today.weekday() == 6:
+                filter_kwars["date"] = today + timedelta(days=1)
+            else:
+                filter_kwars["date"] = today
+
+        bookings = [
+            {
+                "id": booking.id,
+                "lesson_id": booking.lesson.id,
+                "date": booking.date,
+                "period": booking.period,
+                "subject": {
+                    "id": booking.lesson.subject.id,
+                    "name": booking.lesson.subject.short,
+                },
+                "teachers": [
+                    {
+                        "id": teacher.id,
+                        "name": teacher.short,
+                    }
+                    for teacher in booking.lesson.teachers.all()
+                ],
+                "groups": [
+                    {
+                        "id": group.id,
+                        "name": group.name,
+                    }
+                    for group in booking.lesson.groups.all()
+                ],
+                "classroom": {
+                    "id": booking.classroom.id,
+                    "name": booking.classroom.name,
+                }
+                if booking.classroom
+                else None,
+            }
+            for booking in models.Booking.objects.filter(**filter_kwars).order_by("date")
+        ]
 
         return Response(bookings, status=status.HTTP_200_OK)
 
     """Interacts with incoming 'xml'file """
+
     def create(self, request):
-        week = request.data.get('week')
-        _file = request.data.get('file')
+        week = request.data.get("week")
+        _file = request.data.get("file")
 
         import_timetable(week, _file)
-        cache.delete('last_modified')
+        cache.delete("last_modified")
 
-        msg = {'message': 'all bookings are successfully stored'}
+        msg = {"message": "all bookings are successfully stored"}
         return Response(msg, status=status.HTTP_201_CREATED)
 
 
 class UserViewSet(ViewSet):
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAdminUser,)
 
     """manages users"""
+
     def list(self, request):
         """returns all students in our database"""
         users = get_user_model().objects.all()
         serializer = serializers.UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
     def create(self, request):
         """handles incoming student list"""
-        csv_file = request.data.get('file')
+        csv_file = request.data.get("file")
 
         import_students(csv_file)
 
         msg = {"message": "student list have successfully been stored"}
         return Response(msg, status=status.HTTP_201_CREATED)
 
+
 class TeacherSubjectViewSet(ViewSet):
     """importing teachers"""
+
     def create(self, request):
-        csv_file = request.data.get('file')
+        csv_file = request.data.get("file")
         import_teacher_lesson(csv_file)
         return Response(status=status.HTTP_200_OK)
 
+
 class LevelSubjectViewSet(ViewSet):
     def list(self, request):
-        level = request.data.get('level')
-        term = request.data.get('term')
+        level = request.data.get("level")
+        term = request.data.get("term")
         objects = models.TeacherSubject.objects.filter(level=level, term=term)
         subjects = []
         for obj in objects:
@@ -260,39 +310,47 @@ class LevelSubjectViewSet(ViewSet):
                 subjects.append(obj.subject.name)
         data = dict(subjects=subjects)
         return Response(data, status=status.HTTP_200_OK)
+
+
 class LevelTeacherViewSet(ViewSet):
     def list(self, request):
-        subject = request.data.get('subject')
+        subject = request.data.get("subject")
         objects = models.TeacherSubject.objects.filter(subject__name=subject)
         teachers = []
         for obj in objects:
-            if obj.teacher.firstname + ' ' + obj.teacher.lastname not in teachers:
-                teachers.append(obj.teacher.firstname + ' ' + obj.teacher.lastname)
+            if obj.teacher.firstname + " " + obj.teacher.lastname not in teachers:
+                teachers.append(obj.teacher.firstname + " " + obj.teacher.lastname)
         data = dict(teachers=teachers)
         return Response(data, status=status.HTTP_200_OK)
+
 
 class AvailableRoomViewSet(ViewSet):
     def list(self, request):
         """returns all available rooms for today"""
         # start_date = date.today()
         # end_date = start_date + timedelta(5-start_date.weekday())
-        day = request.data.get('date')
+        day = request.data.get("date")
         # list of rooms which key is classroom and period__max and value is id and actual period
-        rooms = list(models.Booking.objects.filter(date=day).values('classroom', 'date').annotate(Max('period')))
+        rooms = list(models.Booking.objects.filter(date=day).values("classroom", "date").annotate(Max("period")))
         # filter available rooms
         available_rooms = []
         for room in rooms:
-            if int(room['period__max']) < 6:
-                classroom = models.Classroom.objects.get(id=room['classroom']).name
-                if (classroom[:1].isnumeric() or classroom[:3] == 'LAB'):
-                    available_rooms.append(dict(classroom=classroom,
-                                                starting_time=period_in_time[int(room['period__max'])+1][:5],
-                                                date=room['date']))
+            if int(room["period__max"]) < 6:
+                classroom = models.Classroom.objects.get(id=room["classroom"]).name
+                if classroom[:1].isnumeric() or classroom[:3] == "LAB":
+                    available_rooms.append(
+                        dict(
+                            classroom=classroom,
+                            starting_time=period_in_time[int(room["period__max"]) + 1][:5],
+                            date=room["date"],
+                        )
+                    )
         return Response(available_rooms, status=status.HTTP_200_OK)
 
 
 class EventViewSet(ViewSet):
     """Interacts with event object"""
+
     def create(self, request):
         """Creates and saves a new event"""
         serializer = serializers.EventSerializer(data=request.data)
@@ -300,6 +358,3 @@ class EventViewSet(ViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
